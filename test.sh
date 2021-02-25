6516281a4a3b3d147bfded4e8b3ff2a604aef9e9
AhsanAazmi <58124737+AhsanAazmi@users.noreply.github.com>
2021-02-25 14:07:01 +0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,19 @@
 echo "Helloooooo World"
+
+
+#!/bin/sh
+
+mkdir /update_package/tmp
+echo "dir cr"
+cd /update_package/tmp          
+echo "in dir"
+curl -sS https://github.com/AhsanAazmi/installer_package_test/archive/main.zip > package.zip
+echo "dwn"
+unzip package.zip
+echo "unzip"                                  
+rm package.zip
+echo "rm"
+cd /update_package/package
+echo "in package"
+chmod +x installer.sh
+sh installer.sh
