5a07ca581f7c016988e731e518dee84b8f1d864d
AhsanAazmi <58124737+AhsanAazmi@users.noreply.github.com>
2021-02-25 11:44:49 +0500

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,8 +1,1 @@
-my_string="Ubuntu;Linux Mint;Debian;Arch;Fedora"
-IFS=';' read -ra my_array <<< "$my_string"
-
-#Print the split string
-for i in "${my_array[@]}"
-do
-    echo $i
-done
+echo "Helloooooo"
